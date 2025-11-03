7c949ae2d390fffa74fdbeefba6f80e79938bebf
Ameed_Ahmed <46124638+ameedahmed@users.noreply.github.com>
2025-11-03 10:52:31 +0100

    Updated table
    Removed detailed payment type case statement from Location_Information.sql.

--- models/star_schema/Location_Information.sql
+++ models/star_schema/Location_Information.sql
@@ -6,14 +6,6 @@ select
     payment_type, 
     PULocationID AS Pickup_Taxi_zone,
     DOLocationID AS Dropoff_Taxi_zone,
-    CASE WHEN payment_type == '0' THEN 'Flex Fare Trip'
-    WHEN payment_type =='1' THEN 'Credit Card'
-    WHEN payment_type =='2' THEN 'Cash'
-    WHEN payment_type =='3' THEN 'No Charge'
-    WHEN payment_type =='4' THEN 'Dispute'
-    WHEN payment_type =='5' THEN 'Unknown'
-    WHEN payment_type =='6' THEN 'Voided Trip'
-    ELSE 'Other' END AS payment_type_detail,
     CAST(RatecodeID as string) AS RatecodeID,
     CASE WHEN RatecodeID =='1' THEN 'Standard Rate'
     WHEN RatecodeID =='2' THEN 'JFK'
